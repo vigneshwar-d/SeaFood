7284b428979e713592bbbf2092d63919dfec7478
Vigneshwar Devendran <sendtovikki@outlook.com>
2018-04-06 13:33:14 +0530

    Initial Commit

---
+++ SeaFood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SeaFood
+//
+//  Created by Vigneshwar Devendran on 06/04/18.
+//  Copyright © 2018 Vigneshwar Devendran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
